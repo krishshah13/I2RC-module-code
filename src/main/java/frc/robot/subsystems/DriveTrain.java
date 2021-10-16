e5337f471e0fe2faf8b418c91e38c0e5461ba812
krishshah13 <krishsshah8@gmail.com>
2021-10-16 14:19:29 -0700

    Update DriveTrain.java

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,17 +4,20 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import com.kauailabs.navx.frc.AHRS;
 
 public class DriveTrain extends SubsystemBase {
 
   private final WPI_TalonSRX _leftDriveTalon;
   private final WPI_TalonSRX _righttDriveTalon;
+  private AHRS navx = new AHRS(SPI.Port.kMXP);
 
   private DifferentialDrive _diffDrive;
 
@@ -29,14 +32,38 @@ public class DriveTrain extends SubsystemBase {
 
     _diffDrive = new DifferentialDrive(_leftDriveTalon, _righttDriveTalon);
 
+  //talon configuration
+  _leftDriveTalon.configFactoryDefault();
+  _leftDriveTalon.setInverted(false);
+  _leftDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+
+  _righttDriveTalon.configFactoryDefault();
+  _righttDriveTalon.setInverted(false);
+  _righttDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+
+
 
   }
+
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     //arcadeDrive(-0.8 * _joystick.getRawAxis(Constants.JoystickAxis.YAxis);
   }
+
+  public void resetEncoders() {
+    _leftDriveTalon.setSelectedSensorPosition(0, 0, 10);
+    _righttDriveTalon.setSelectedSensorPosition(0, 0, 10);
+  }
+
+public double getDistance() {
+  return ((_leftDriveTalon.getSelectedSensorPosition(0) + _righttDriveTalon.getSelectedSensorPosition(0))/2) * "find constant";
+}
+
+public double getVelocity() {
+  return ((_leftDriveTalon.getSensorCollection().getPulseWidthVelocity() + _righttDriveTalon.getSensorCollection().getPulseWidthVelocity())/2) * "find constant";
+}
 
   public void tankDrive(double leftSpeed, double rightSpeed) {
     _diffDrive.tankDrive(leftSpeed, rightSpeed);
